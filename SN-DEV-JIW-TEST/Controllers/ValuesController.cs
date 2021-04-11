2bc453344169af6f2b3539d21e879c0e03928d06
Lisandro Rossi <Lisandrorossi444@gmail.com>
2021-04-11 19:15:52 -0300

    harcoded the realm not getting realm from service.

--- SN-DEV-JIW-TEST/Controllers/ValuesController.cs
+++ SN-DEV-JIW-TEST/Controllers/ValuesController.cs
@@ -135,7 +135,7 @@ namespace SN_DEV_JIW_TEST.Controllers
 
                 Uri siteUri = new Uri(provisioningUrl);
                 //Get the realm for the URL
-                string realm = TokenHelper.GetRealmFromTargetUrl(siteUri);
+                string realm = "9d89ee4f-c590-47c4-be02-c3fc82611185"; //string realm = TokenHelper.GetRealmFromTargetUrl(siteUri);
                 string HostedAppHostName = "JIW"; // WebConfigurationManager.AppSettings.Get("HostedAppHostName");
                 string resource = GetFormattedPrincipal(TokenHelper.SharePointPrincipal, siteUri.Authority, realm);
                 string clientIdFormated = GetFormattedPrincipal(clientId, HostedAppHostName, realm);
@@ -147,8 +147,8 @@ namespace SN_DEV_JIW_TEST.Controllers
                 //Get the access token for the URL.  
                 //   Requires this app to be registered with the tenant
 
-                TokenHelper.ClientId = "8d8c127f-6a3c-48a5-9538-0b329f5eb5fe";
-                TokenHelper.ClientSecret = "ERNLnW6K11vlwf21wZ4i8KbiiKCZmUNOrJgmWHlY/wU=";
+                //TokenHelper.ClientId = "8d8c127f-6a3c-48a5-9538-0b329f5eb5fe";
+                //TokenHelper.ClientSecret = "ERNLnW6K11vlwf21wZ4i8KbiiKCZmUNOrJgmWHlY/wU=";
                 string accessToken = TokenHelper.GetAppOnlyAccessToken(TokenHelper.SharePointPrincipal, siteUri.Authority, realm).AccessToken;
 
                 string userAgent = "NONISV|JohnsonControls|SelNav.Integration.Web/1.0";
